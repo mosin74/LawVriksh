7042a43bbe2430ff074f67515f0e3023cb6f7513
mosin74 <skmosin70201566@gmail.com>
2025-07-21 15:48:44 +0530

    Deployment issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import './app.css';
+import './App.css';
 import App from './App.jsx';
 import { CardProvider } from './context/CardContext';
 import { ActivityProvider } from './context/ActivityContext.jsx';
